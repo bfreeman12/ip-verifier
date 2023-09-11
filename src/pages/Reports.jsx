574233c8cbaac8913925da05c03a9ffa4b20e2d2
Brandon Freeman <bfreeman@mdt-clear-04.samfoxcyber.space>
2023-09-11 16:43:13 +0000

    adding design for reports page fixed colors on home page

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,12 +1,89 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../styles/reports.css";
 
 export default function Reports() {
   return (
     <>
       <Navbar />
 
-      <div className="body">Reports page</div>
+      <div className="body">
+        <h1>Reports</h1>
+        <div className="entries">
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+          <div className="entry">
+            <p>12/1212</p>
+            <p>this is label</p>
+            <a>this is link</a>
+            <p>banana</p>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
