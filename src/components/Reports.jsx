c766e4a7e3b2b179ad74297d45a03406b7697f2e
Brandon Freeman <bfreeman@mdt-clear-04.samfoxcyber.space>
2023-09-18 19:07:36 +0000

    added conditional rendering

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -10,44 +10,45 @@ const Reports = () => {
       label: "this is a label",
       link: "this is a link",
       banana: "banana",
-      threat:'low'
+      threat: "low",
     },
     1: {
       date: "12/12/1212",
       label: "this is a label",
       link: "this is a link",
       banana: "banana",
-      threat:'low'
+      threat: "low",
     },
     2: {
       date: "12/12/1212",
       label: "this is a label",
       link: "this is a link",
       banana: "banana",
-      threat:'low'
+      threat: "low",
     },
     3: {
       date: "12/12/1212",
       label: "this is a label",
       link: "this is a link",
       banana: "banana",
-      threat:'low'
+      threat: "low",
     },
     4: {
       date: "12/12/1212",
       label: "this is a label",
       link: "this is a link",
       banana: "banasdadana",
-      threat:'high'
+      threat: "high",
     },
     5: {
       date: "12/12/1212",
       label: "this is a label",
       link: "this is a link",
       banana: "banana",
-      threat:'low'
+      threat: "low",
     },
   });
+  let content;
 
   const removeElement = (e) => {
     e.preventDefault();
@@ -59,25 +60,26 @@ const Reports = () => {
     setData(newData);
   };
 
-  return (
-    <div className="entries">
-      {Object.keys(data).map((key, index) => {
-        const entry = data[key];
-        return (
-          <div className="entry" key={index}>
-            <p>{entry.date}</p>
-            <p>{entry.label}</p>
-            <p>{entry.link}</p>
-            <p>{entry.banana}</p>
-            <p>{entry.threat}</p>
+  if (Object.keys(data).length > 0) {
+    content = Object.keys(data).map((key, index) => {
+      const entry = data[key];
 
-            <button id={key} onClick={(e) => removeElement(e)}>
-              <FontAwesomeIcon icon={faTrashAlt} />
-            </button>
-          </div>
-        );
-      })}
-    </div>
-  );
+      return (
+        <div className="entry" key={index}>
+          <p>{entry.date}</p>
+          <p>{entry.label}</p>
+          <p>{entry.link}</p>
+          <p>{entry.banana}</p>
+          <p>{entry.threat}</p>
+          <button id={key} onClick={(e) => removeElement(e)}>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </button>
+        </div>
+      );
+    });
+  } else {
+    content = "No entries found";
+  }
+  return <div className="entries">{content}</div>;
 };
 export default Reports;
